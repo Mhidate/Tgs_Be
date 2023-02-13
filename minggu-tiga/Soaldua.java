c0fe3c15c3fd696bc59b96f27c879785db1a855b
Muhammad Hidayatullah <72199625+Mhidate@users.noreply.github.com>
2023-02-13 19:04:35 +0800

    Create Soaldua.java

---
+++ minggu-tiga/Soaldua.java
@@ -0,0 +1,34 @@
+public class Soaldua {
+  public static void main(String[] args) {
+    //r jari-jari
+   // rumus Luas lingkaran = π × r²
+    
+   //variabel untuk luas lingkaran
+    double r = 2.5, phi = 3.14 , luaslingkaran = phi * r * r;
+    
+    
+   // rumus volume balok = panjang * lebar * tinggi 
+    
+    //variabel untuk volume balok
+    double p = 6, l = 3, t = 4, volume = p*l*t; 
+
+    System.out.println("--------------------------------------------------------");
+    System.out.println("Soal no 2 luas lingkaran");
+    System.out.println();
+    System.out.println("Rumus Luas lingkaran = π * r²");
+    System.out.println(" π (phi) = " + phi);
+    System.out.println(" r = " + r);
+    System.out.println(" Luas Lingkaran = "+luaslingkaran);
+    System.out.println("--------------------------------------------------------");
+    System.out.println();
+    System.out.println("--------------------------------------------------------");
+    System.out.println("Soal no 2 volume balok");
+    System.out.println();
+    System.out.println("Rumus volume balok = panjang * lebar * tinggi ");
+    System.out.println(" Panjang = " + p);
+    System.out.println(" Lebar = " + l);
+    System.out.println(" Tinggi = " + t);
+    System.out.println(" Volume balok = " + volume );
+    System.out.println("--------------------------------------------------------");
+  }
+}
