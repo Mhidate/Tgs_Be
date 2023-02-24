b6e06b35a454479893457a03b6d8b68b0a0dc03b
Muhammad Hidayatullah <72199625+Mhidate@users.noreply.github.com>
2023-02-24 15:20:00 +0800

    Create Lingkaran.java

---
+++ minggu-lima/Lingkaran.java
@@ -0,0 +1,21 @@
+public class Lingkaran {
+
+    double r,l,k;
+    double phi = 3.14;
+
+    void hitungli(){
+    
+      l = phi*r*r;
+      k= phi*2*r;
+      System.out.println("---------------");
+      System.out.println("Rumus luas lingkaran L = π(phi) x r x r");
+      System.out.println("Rumus Keliling lingkaran Kll = π x 2 x r");
+      System.out.println("Jari-jari = "+ r);
+      System.out.println("Luas lingkaran = "+l);
+      System.out.println("Keliling lingkaran = "+k);
+
+    // refrensi rumus https://www.advernesia.com/blog/matematika/rumus-keliling-lingkaran-dan-rumus-luas-lingkaran/
+    
+ }  
+
+}
