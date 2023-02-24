b0b596e77080737c59aa01635af4189c5bdedd8a
Muhammad Hidayatullah <72199625+Mhidate@users.noreply.github.com>
2023-02-24 15:19:22 +0800

    Create Segitigasembarang.java

---
+++ minggu-lima/Segitigasembarang.java
@@ -0,0 +1,27 @@
+
+public class Segitigasembarang {
+
+    Integer palas,tinggi,kelilig,sa,sb,sc;
+    Double luas;
+    
+    void hitungsg(){
+    
+        luas = 0.5 *palas*tinggi;
+        kelilig= sa+sb+sc;
+        System.out.println("---------------");
+        System.out.println("Rumus Luas segitiga sembarang L = ½ x alas x tinggi");
+        System.out.println("Rumus Keliling segitiga sembarang = sisi a + sisi b + sisi c");
+        System.out.println("");
+        System.out.println("Panjang alas = " + palas);
+        System.out.println("Tinggi = " + tinggi);
+        System.out.println("sisi A,B,C = "+sa+","+sb+","+sc);
+        System.out.println("Luas Segitiga sembarang = "+ luas);
+        System.out.println("Keliling segitiga sembarang = "+ kelilig);
+
+        // refrensi rumus https://www.doyanblog.com/segitiga-sembarang/
+
+    }
+    }
+    
+    
+
