57ad66278f89c73b88af36d9d44ebd779e01ca6c
Muhammad Hidayatullah <72199625+Mhidate@users.noreply.github.com>
2023-02-24 15:17:28 +0800

    Create Persegi.java

---
+++ minggu-lima/Persegi.java
@@ -0,0 +1,21 @@
+
+public class Persegi {
+
+    Integer s,l,k;
+
+    void hitungper(){
+    
+        l = s*s;
+        k= s*s*s*s;
+        System.out.println("---------------");
+        System.out.println("Rumus luas persegi = Sisi x Sisi");
+        System.out.println("Rumus Keliling persegi= Sisi x Sisi x Sisi x Sisi");
+        System.out.println("Sisi = "+ s);
+        System.out.println("Luas persegi = "+l);
+        System.out.println("Keliling persegi = "+k);
+
+        //refrensi rumus https://www.belajarmtk.com/rumus-persegi-luas-persegi-dan-keliling-persegi-lengkap-contoh-soalnya/
+
+    }
+    
+}
